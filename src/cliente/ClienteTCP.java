2ae14e91bf845f991a19db45193fa4107b8a62d5
Dagyss <depa.agustin@gmail.com>
2025-03-13 16:41:24 -0300

    feat: Implementada funcionalidad que permite la reconexion y el envio del saludo nuevamente en el caso de que el proceso Servidor cierre la conexion.

--- src/cliente/ClienteTCP.java
+++ src/cliente/ClienteTCP.java
@@ -9,22 +9,30 @@ import java.net.Socket;
 public class ClienteTCP {
     public static void main(String[] args) {
 
-        String serverAddress = System.getenv("DIRECCION_SERVIDOR"); // Set from environment variable
+        String serverAddress = System.getenv("DIRECCION_SERVIDOR");
         if (serverAddress == null) {
-            serverAddress = "localhost"; // Default value if environment variable is not set
+            serverAddress = "localhost";
         }
         int port = 5000;
 
-        try (Socket socket = new Socket(serverAddress, port)) {
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        while (true) {
+            try (Socket socket = new Socket(serverAddress, port)) {
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                output.println("Hola, servidor!");
+                String response = input.readLine();
+                System.out.println("Respuesta del servidor: " + response);
+                break;
+            } catch (IOException e) {
+                System.out.println("Error al intentar conectar o durante la comunicación. Intentando nuevamente...");
 
-            output.println("Hola, servidor!");
-            String response = input.readLine();
-            System.out.println("Respuesta del servidor: " + response);
-        } catch (IOException e) {
-            e.printStackTrace();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    System.out.println("Error durante la espera de reconexión");
+                }
+            }
         }
     }
-
 }
