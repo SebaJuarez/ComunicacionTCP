e9289855e0d74ce2d35b8b1ca44021cc4b5200dd
Dagyss <depa.agustin@gmail.com>
2025-03-23 21:50:21 -0300

    test: agregado auto-test hit7

--- src/scripts/test_hit7.sh
+++ src/scripts/test_hit7.sh
@@ -1,5 +1,5 @@
 # Configuración
-FILAS=20
+FILAS=200
 ESPERA=10  # Ajustado para el ciclo de actualización de ventanas (20s)
 
 echo "🔄 Construyendo y levantando los contenedores..."
@@ -24,17 +24,13 @@ echo "| log nodo3:                            |"
 docker logs --tail $FILAS nodo3
 echo "-----------------------------------------"
 
-# Verificar el archivo de inscripciones
-echo "📂 Verificando contenido del archivo de inscripciones..."
-cat logs-registros/inscripciones.json || echo "⚠ No se encontró el archivo de inscripciones."
 sleep $ESPERA
 
-# Simulación de desconexión y reconexión de nodos
 echo "⏸ Deteniendo 'nodo1' y 'nodo2'..."
 docker stop nodo1 || { echo "Error: No se pudo detener 'nodo1'"; exit 1; }
 docker stop nodo2 || { echo "Error: No se pudo detener 'nodo2'"; exit 1; }
-sleep $ESPERA
 
+sleep $ESPERA
 echo "▶ Iniciando 'nodo1' y 'nodo2'..."
 docker start nodo1 || { echo "Error: No se pudo iniciar 'nodo1'"; exit 1; }
 docker start nodo2 || { echo "Error: No se pudo iniciar 'nodo2'"; exit 1; }
@@ -44,7 +40,7 @@ sleep $ESPERA
 echo "📜 Logs después de reiniciar nodo1 y nodo2:"
 echo "-----------------------------------------"
 echo "| log registro_contactos:               |"
-docker logs --tail $FILAS registro_contactos
+docker logs --tail 6 registro_contactos
 echo "-----------------------------------------"
 echo "| log nodo1:                            |"
 docker logs --tail $FILAS nodo1
@@ -52,9 +48,11 @@ echo "-----------------------------------------"
 echo "| log nodo2:                            |"
 docker logs --tail $FILAS nodo2
 echo "-----------------------------------------"
-
+sleep 21
 # Verificar nuevamente el archivo de inscripciones
 echo "📂 Verificando contenido del archivo de inscripciones..."
-cat logs-registros/inscripciones.json || echo "⚠ No se encontró el archivo de inscripciones."
-
+cat ../../logs-registros/inscripciones.json || echo "⚠ No se encontró el archivo de inscripciones."
+echo " "
 echo "✅ Pruebas completadas."
+sleep $ESPERA
+docker-compose down
